d51e6fa8c6aaa984d12c191fcf72b95bba17f95e
Tom <tom.larson04@gmail.com>
2024-01-09 19:32:18 +1000

    man i hate javascript

--- Assignment 2 Milestone 1/q1/q1.js
+++ Assignment 2 Milestone 1/q1/q1.js
@@ -6,34 +6,50 @@ const SCALE = 1;
 const BLACK = 0;
 const WHITE = 255;
 
+// Issues with the eye functions - wanting to improve on previous implimentations
+// Exploring vectors and potitions around a circle, looking into clamping a vector to
+// a scalar around the edge of a circle
+
 const burgers = [];
+const burgersInfo = []; //Extra info for each member of burgers
 const numberOfBurgers = 4;
 
 let burgerSprite;
+
 let score = 0;
+
+let debug;
+const Debug = false;
 
 
 const Burger = {
     speed: 5,
     collider: 'k',
+    inMouth: false,
 }
 
 const Ralph = {
     width: 250,
-    height: 260,
+    height: 280,
     x: 50,
     y: CANVASHEIGHT / 4,
 
     mouth: {
-        x: 150,
-        y: CANVASHEIGHT / 2,
-        width: 150,
+        x: 0,
+        y: 0,
+        width: 0,
         height: 0,
     },
     eye: {
-        width: 50,
-        x: 150,
-        y: 700,
+        width: 0,
+        x: 0,
+        y: 0,
+    },
+    pupil: {
+        size: 0,
+        offset: 0,
+        x: 0,
+        y: 0,
     },
     nose: {
         x: [],
@@ -42,6 +58,7 @@ const Ralph = {
     jawPosition: 0,
     hairLine: 0,
     curve: 40,
+    speed: 3,
 }
 
 function preload() {
@@ -51,6 +68,7 @@ function preload() {
 function setup() {
     createCanvas(CANVASWIDTH, CANVASHEIGHT);
     initialiseBurgers();
+    initialiseRalph();
 }
 
 function draw() {
@@ -58,6 +76,7 @@ function draw() {
     strokeWeight(2);
     stroke(BLACK);
     update();
+    noFill();
 
 
     fill(WHITE)
@@ -65,13 +84,28 @@ function draw() {
 
     fill(BACKGROUNDCOLOUR)
     rect(Ralph.mouth.x, Ralph.mouth.y, Ralph.mouth.width, Ralph.mouth.height + Ralph.jawPosition);
-    stroke(BACKGROUNDCOLOUR)
+    stroke(BACKGROUNDCOLOUR);
+    strokeWeight(3);
     line(Ralph.mouth.x + Ralph.mouth.width, Ralph.mouth.y, Ralph.mouth.x + Ralph.mouth.width, Ralph.mouth.y + Ralph.jawPosition)
 
+    stroke(BLACK)
+    fill(WHITE)
+    ellipse(Ralph.eye.x, Ralph.eye.y, Ralph.eye.width);
+
     fill(BLACK)
-    text(score, 50, 50)
-    //text(Burger[0].prevPositionX, 50, 75)
-    lateUpdate();
+    ellipse(Ralph.pupil.x, Ralph.pupil.y, Ralph.pupil.size);
+    rect(Ralph.x, Ralph.y, Ralph.width, Ralph.hairLine, Ralph.curve, Ralph.curve, 0 ,0);
+
+    if (Debug == true) {
+        line(Ralph.eye.x, Ralph.eye.y, burgers[0].x, burgers[0].y)
+        line(Ralph.eye.x, 0, Ralph.eye.x, Ralph.eye.y)
+        line(burgers[0].x, burgers[0].y, Ralph.eye.x, 0)
+        text(debug, 50, 75)
+    }
+    strokeWeight(1);
+
+    text("Burgers eaten: ", 30, 50)
+    text(score, 120, 50)
 }
 
 function initialiseBurgers() { // make velocity skewed towards x
@@ -91,61 +125,86 @@ function initialiseBurgers() { // make velocity skewed towards x
         burger.drag = 0;
         burger.direction = d;
 
-        burger.debug = true;
+        burger.inMouth = false;
+
+        //burger.debug = true;
+
+        burgersInfo.push(Burger);
 
         burgers.push(burger);
     }
 }
 
+function initialiseRalph() {
+    Ralph.mouth.width = Ralph.width / 1.5;
+    Ralph.mouth.height = 0;
+    Ralph.mouth.x = Ralph.width - Ralph.mouth.width + Ralph.x;
+    Ralph.mouth.y = Ralph.y * 2;
+
+    Ralph.eye.width = Ralph.width / 5;
+    Ralph.eye.x = Ralph.x + (Ralph.width * 0.75);
+    Ralph.eye.y = Ralph.y + (Ralph.height * 0.35);
+
+    Ralph.pupil.size = Ralph.eye.width / 2;
+    Ralph.pupil.offset = Ralph.eye.width / 4;
+    Ralph.pupil.x = Ralph.eye.x;
+    Ralph.pupil.y = Ralph.eye.y;
+
+    Ralph.hairLine = Ralph.height / 5;
+}
+
 function update() {
     // put all non-drawing functions called every frame here
     moveRalph();
-    boundaryCollision();
+    getPupilDirection();
+    moveRalphPupil();
     Ralph.jawPosition = getJawPosition();
-}
-
-function lateUpdate() {
+    boundaryCollision();
 
 }
 
 function boundaryCollision() { // still weird especially with moving ralph
-    let burgerInMouth = false;
     for (i = 0; i < numberOfBurgers; i++) { 
-        if (burgers[i].x < Ralph.x + Ralph.width - 1) {
-            burgerInMouth = true;
+        if (burgers[i].x - (burgers[i].w / 2)< Ralph.x + Ralph.width - 10) {
+            burgersInfo[i].inMouth = true;
         } else {
-            burgerInMouth = false;
+            burgersInfo[i].inMouth = false;
+        }
+
+        if (burgers[i].x - (burgers[i].w / 2) < Ralph.x + Ralph.width && burgers[i].y - (burgers[i].h / 2) < Ralph.mouth.y && // Hits Ralph above mouth
+            burgersInfo[i].inMouth == false) {
+            burgers[i].x = Ralph.x + Ralph.width + (burgers[i].w / 2)
+            burgers[i].vel.x *= -1;
+        } else if (burgers[i].x - (burgers[i].w / 2) < Ralph.x + Ralph.width &&  // Hits Ralph below mouth
+            burgers[i].y + (burgers[i].h / 2 ) > Ralph.mouth.y + Ralph.jawPosition && burgersInfo[i].inMouth == false) {
+            burgers[i].x = Ralph.x + Ralph.width + (burgers[i].w / 2)
+            burgers[i].vel.x *= -1;
+        }
+
+        if (burgersInfo[i].inMouth == true) {
+            if (burgers[i].y - (burgers[i].h / 2) < Ralph.mouth.y) {
+                burgers[i].y = Ralph.mouth.y + (burgers[i].h / 2);
+                burgers[i].vel.y *= -1;
+            } else if (burgers[i].y + (burgers[i].h / 2) > Ralph.mouth.y + Ralph.jawPosition) {
+                burgers[i].y = Ralph.mouth.y + Ralph.jawPosition - (burgers[i].h / 2);
+                burgers[i].vel.y *= -1;
+            }
+            if (burgers[i].x < Ralph.eye.x) {
+                eatBurger(i);
+            }
         }
 
         if (burgers[i].x + (burgers[i].w / 2) >= CANVASWIDTH) { // x > right wall
             burgers[i].vel.x *= -1;
-        } else if (burgers[i].x - (burgers[i].w / 2) < Ralph.x + Ralph.width) {
-            if (burgers[i].y - (burgers[i].h / 2) < Ralph.mouth.y ||
-            burgers[i].y - (burgers[i].h / 2) > Ralph.mouth.y + Ralph.jawPosition) {
-                burgers[i].vel.x *= -1;
-            } 
-        }      
-
+        }  
+        
         if (burgers[i].y + (burgers[i].h / 2) >= CANVASHEIGHT) { // Top boundary
             burgers[i].vel.y *= -1;
         } else if (burgers[i].y - (burgers[i].h / 2) <= 0) { // Bottom boundary
             burgers[i].vel.y *= -1;
         }
-
-        if (burgerInMouth == true) {
-            if (burgers[i].y - (burgers[i].h / 2) <= Ralph.mouth.y) { // y on roof of mouth
-                burgers[i].vel.y *= -1;
-            } if (burgers[i].y - (burgers[i].h / 2) >= Ralph.mouth.height + Ralph.jawPosition) { // y on bottom of mouth
-                burgers[i].vel.y *= -1;
-            }
-        }
-
-        if (burgers[i].x < Ralph.x + (Ralph.width / 2)) {
-            eatBurger(i);
-        }
     }
 }
-
 
 function randomiseStaringPositions(i) { // fix could do 2 functions for x y
     let r;
@@ -197,8 +256,8 @@ function getJawPosition() {
 }
 
 function eatBurger(b) {
-
     score++;
+
     burgers[b].x = CANVASWIDTH /2;
     burgers[b].y = CANVASHEIGHT / 2;
 
@@ -211,13 +270,47 @@ function eatBurger(b) {
 
 function moveRalph() {
     if (kb.pressing('down')) {
-        Ralph.y += 2;
-        Ralph.mouth.y += 2;
+        Ralph.y += Ralph.speed;
+        Ralph.mouth.y += Ralph.speed;
+        Ralph.eye.y += Ralph.speed;
     }
     if (kb.pressing('up')) {
-        Ralph.y -= 2;
-        Ralph.mouth.y -= 2;
+        Ralph.y -= Ralph.speed;
+        Ralph.mouth.y -= Ralph.speed;
+        Ralph.eye.y -= Ralph.speed;
     }
+}
+
+function getPupilDirection() {
+    let a = CANVASWIDTH;
+    let b, c;
+    let angle;
+
+    for (i = 0; i < numberOfBurgers; i++) {
+        if (burgers[i].x < a) {
+            a = burgers[i].x;
+            b = burgers[i].y; 
+        }
+    } 
+
+    a = Ralph.eye.x - a;
+    b = Ralph.eye.y - b;
+
+
+    c = sqrt(sq(a) + sq(b));
+
+    angle = acos((sq(b) + sq(c) - sq(a)) / (2 * b * c))  - 90
+    debug = angle;
+
+    return angle;
+}
+
+function moveRalphPupil() {
+    let angle;
+    angle = getPupilDirection();
+    Ralph.pupil.x = Ralph.eye.x + Ralph.pupil.offset * cos(angle);
+
+    Ralph.pupil.y = Ralph.eye.y + Ralph.pupil.offset * sin(angle);
 }
 
 
--- Assignment 2 Milestone 1/q2/q2.js
+++ Assignment 2 Milestone 1/q2/q2.js
@@ -1,10 +1,207 @@
 // Rapid roll
-// Draw sprites
+
+const CANVASHEIGHT = 180;
+const CANVASWIDTH = 250;
+const SCALE = 4;
+
+// create 4-5 spikes and platform sprites and hide them when off screen and then move them to the bottom when called
+
+let effectiveCanvasHeight = CANVASHEIGHT * SCALE;
+let effectiveCanvasWidth = CANVASWIDTH * SCALE;
+
+const BACKGROUNDCOLOUR = [189, 185, 89];
+const NONBLINDINGCOLOUR = 150
+
+let ballSprite, zeroSprite, oneSprite, twoSprite, threeSprite,
+fourSprite, fiveSprite, sixSprite, sevenSprite, eightSprite, nineSprite,
+death1Sprite, death2Sprite, heartSprite, livesSprite, platformSprite,
+floorSpikesSprite, roofSpikesSprite, wallSprite, xSprite
+ //probably dont need wall / platform
+
+let topBoundary, leftBoundary, rightBoundary, leftWall, rightWall;
+
+let Spikes = [];
+let Platforms = []
+
+let ScoreBoard = {
+    lives: 2,
+    score: 0, // int
+    digits: [0, 0, 0, 0, 0, 0, 0, 0], // array of ints
+    sprites: [], // Array of sprites
+    scorePosition: 0,
+}
+
+function preload() {
+    ballSprite = loadImage("Sprites/ball.png");
+
+    zeroSprite = loadImage("Sprites/0.png");
+    oneSprite = loadImage("Sprites/1.png");
+    twoSprite = loadImage("Sprites/2.png");
+    threeSprite = loadImage("Sprites/3.png");
+    fourSprite = loadImage("Sprites/4.png");
+    fiveSprite = loadImage("Sprites/5.png");
+    sixSprite = loadImage("Sprites/6.png");
+    sevenSprite = loadImage("Sprites/7.png");
+    eightSprite = loadImage("Sprites/8.png");
+    nineSprite = loadImage("Sprites/9.png");
+
+    livesSprite = loadImage("Sprites/lives.png");
+    xSprite = loadImage("Sprites/x.png");
+}
 
 function setup() {
+    new Canvas(CANVASWIDTH * SCALE, CANVASHEIGHT * SCALE);
+    background(NONBLINDINGCOLOUR);
+    //loadSprites();
+    loadGUI();
+    initialisePlatforms();
+    initialiseBall();
+    initialiseScoreBoard();
 
+    world.gravity.y = 0
 }
 
 function draw() {
+    background(NONBLINDINGCOLOUR);
+
+
+    stroke(0);
+    strokeWeight(1 * SCALE);
+    strokeCap(PROJECT);
+
+    //line(leftBoundary, topBoundary, rightBoundary, topBoundary);
+    line(leftBoundary, topBoundary, leftBoundary, effectiveCanvasHeight)
+    line(rightBoundary, topBoundary, rightBoundary, effectiveCanvasHeight);
+
+    line(leftWall, topBoundary, leftWall, effectiveCanvasHeight);
+    line(rightWall,topBoundary, rightWall, effectiveCanvasHeight);
+    //updateScoreBoard();
+
+    text(frameCount, 10, 10)    
+    //moveBall();
+}
+
+function loadGUI() {
+    topBoundary = effectiveCanvasHeight * 0.18 ;
+    leftBoundary = effectiveCanvasWidth * 0.05 ;
+    rightBoundary = effectiveCanvasWidth - leftBoundary;
+
+    leftWall = leftBoundary / 4;
+    rightWall = effectiveCanvasWidth - leftWall;
+}
+
+function initialiseBall() {
+    let Ball = new Sprite();
+    Ball.img = ballSprite;
+    Ball.scale = SCALE;
+    Ball.debug = false;
+    Ball.collider = 'k';
+    Ball.x = 300;
+    Ball.y = 300;
+}
+
+function initialiseSpikes() {
+    let numberOfSpikes = 0; // Get from json
+    for (i = 0; i < numberOfSpikes; i++) {
+        let spikes = new Sprite();
+        spikes.img = floorSpikesSprite;
+        Spikes.push(spikes);
+    }
+}
+
+function initialisePlatforms() {
+    let numberOfPlatforms = 1;
+    for (i = 0; i < numberOfPlatforms; i++) {
+        let platform = new Sprite(effectiveCanvasWidth / 2, effectiveCanvasHeight)
+        platform.h = 100;
+        platform.w = effectiveCanvasWidth;
+        platform.collider = 'static';
+        Platforms.push(platform);
+    }
+}
+
+function initialiseScoreBoard() {
+    n = 8
+    for (i = 0; i < n; i++) {
+        let digit = new Sprite(zeroSprite);
+        //digit.img = zeroSprite;
+        digit.scale = SCALE * 2.5
+        digit.pixelPerfect = true;
+        digit.debug = false;
+        ScoreBoard.scorePosition = rightBoundary - (digit.w * 6.5)
+        digit.y = topBoundary / 2;
+        digit.x = ScoreBoard.scorePosition + (digit.w * i * 0.9);
+        digit.collider = 'k';
+        digit.addImage
+
+
+        ScoreBoard.sprites.push(digit);
+    }
+
+    let livesSymbol = new Sprite();
+    livesSymbol.img = livesSprite;
+    livesSymbol.x = leftBoundary;
+    livesSymbol.y = topBoundary / 2;
+    livesSymbol.scale = SCALE * 2.5;
+    livesSymbol.collider = 'k';
+
+
+    let xSymbol = new Sprite();
+    xSymbol.img = xSprite;
+    xSymbol.x = leftBoundary + livesSymbol.w;
+    xSymbol.y = topBoundary / 2;
+    xSymbol.scale = SCALE * 2.5;
+    xSymbol.collider = 'k';
+
+    let livesValueSymbol = new Sprite();
+    livesValueSymbol.img = twoSprite;
+    livesValueSymbol.x = leftBoundary + (livesSymbol.w * 2);
+    livesValueSymbol.y = topBoundary / 2;
+    livesValueSymbol.scale = SCALE * 2.5;
+    livesValueSymbol.collider = 'k';
 
 }
+
+function moveBall() {
+    Ball.moveTowards(mouse, 0.10)
+}
+
+function updateScoreBoard() {
+    let n;
+    for (i = 0; i < ScoreBoard.digits.length(); i++) {
+        n = 10 * (ScoreBoard.digits.length() - i);
+        ScoreBoard.digits[i] = ScoreBoard.score / n;
+        switch(ScoreBoard.digits[i]) {
+            case 0:
+                ScoreBoard.sprites[i].img = zeroSprite;
+                break;
+            case 1:
+                ScoreBoard.sprites[i].img = oneSprite;
+                break;
+            case 2:
+                ScoreBoard.sprites[i].img = twoSprite;
+                break;
+            case 3:
+                ScoreBoard.sprites[i].img = threeSprite;
+                break;
+            case 4:
+                ScoreBoard.sprites[i].img = fourSprite;
+                break;
+            case 5:
+                ScoreBoard.dispritesgits[i].img = fiveSprite;
+                break;
+            case 6:
+                ScoreBoard.sprites[i].img = sixSprite;
+                break;
+            case 7:
+                ScoreBoard.sprites[i].img = sevenSprite;
+                break;
+            case 8:
+                ScoreBoard.sprites[i].img = eightSprite;
+                break;
+            case 9:
+                ScoreBoard.sprites[i].img = nineSprite;
+                break;
+        }
+    }
+} 
